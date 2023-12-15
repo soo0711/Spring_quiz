eee30a9380278779a696fcb7d9861827b75592ae
test <test@test.com>
2023-12-15 18:44:19 +0900

    quiz lesson02 끝

---
+++ src/main/java/com/quiz/lesson02/bo/StoreBO.java
@@ -0,0 +1,23 @@
+package com.quiz.lesson02.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson02.domain.Store;
+import com.quiz.lesson02.mapper.StoreMapper;
+
+@Service
+public class StoreBO {
+	
+	// field
+	@Autowired
+	private StoreMapper storeMapper;
+	
+	// input: X
+	// output: List<Store> => 컨트롤러 한테
+	public List<Store> getStoreList(){
+		return storeMapper.selectStoreList();
+	}
+}
